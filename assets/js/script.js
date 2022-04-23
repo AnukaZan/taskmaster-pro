639a89b4237f3c98b74a8cb916df82c04f45cf66
Anuka <anukazan@gmail.com>
2022-04-22 23:57:52 -0700

    editing date and description

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,105 @@ var saveTasks = function() {
 };
 
 
+//Clicking on p element turns it into a textarea
+$(".list-group").on("click", "p", function(){
+  var text=$(this)
+    .text() //save the current element's inner text content
+    .trim(); //remove extra white pace
+
+  var textInput = $("<textarea>") //create a new <textarea> element - <> means create
+  //but $("textarea") tells jQuery to find all existing <textarea> elements - basically as a selector
+    .addClass("form-control")  //add the class "form-control"
+    .val(text); //get current value of first element in set of matched elements
+
+  $(this).replaceWith(textInput)  
+
+  textInput.trigger("focus"); //when textInput is triggered, highlight element
+});
+
+$(".list-group").on("blur", "textarea", function(){ // when user interacts with somethin gother than textarea
+  //get the textarea's current value/text
+  var text=$(this)
+    .text()
+    .trim(); //save the text in the element as"text"
+
+  //get parent ul's id attribute
+  var status = $(this) //why is this an array
+    .closest(".list-group") //get first .list-group element by travelling up the ancestors
+    .attr("id") //returns the ID
+    .replace("list-", "");  //remove "list-" from text which gives us the category name (ex "toDo") which matches one of the array (ex tasks.toDo)
+
+  //get the task's position in the list of other li elements  
+  var index = $(this)
+    .closest(".list-group-item") //get first .list-group-item (task item) by travelling up ancestors
+    .index(); //returns index position
+
+  //example output:  text="walk"; status="toDo"; index: 0;  
+
+  tasks[status][index].text = text; //returns the text property of object at given index ex tasks.toDo.[0]
+  saveTasks();
+
+  //recreate p element
+  var taskP = $("<p>") //create new p element as taskP
+    .addClass("m-1")
+    .text(text);
+
+  //replace textarea with p element
+  $(this).replaceWith(taskP);
+});
+
+//DUE DATE WAS CLICKED (span within .list-group)
+$(".list-group").on("click", "span", function(){
+  //get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  //create new input element
+  var dateInput = $("<input>") //create input element
+    .attr("type", "text")  //set type="text"
+    .addClass("form-control")
+    .val(date); //returns input value of date
+
+  //swap out elements  
+  $(this).replaceWith(dateInput); //turn the due dates into text inputs when clicked
+
+  //when triggered, focus on new element
+  dateInput.trigger("focus");
+});
+
+//VALUE OF DUE DATE WAS CHANGED 
+$(".list-group").on("blur", "input[type='text']", function(){ //when you click on anything other than input
+  //get current text
+  var date = $(this)
+    .val()  //returns values of form inputs, select, textarea
+    .trim();
+
+  //  get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group") //parent's ul
+    .attr("id") //get id
+    .replace("list-", ""); 
+
+  // get the task's position in the list of other li elements
+  var index =  $(this)
+    .closest(".list-group-item") 
+    .index(); //get index number
+    
+  //update task in array and re-save to localStorage
+  tasks[status][index].date = date;
+  saveTasks;
+
+  //revert back by recreating new span element with bootstrap classes
+  var taskSpan = $("<span>") //new element
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  //replace input with span element
+  $(this).replaceWith(taskSpan);
+
+});
+
 
 
 // modal was triggered
